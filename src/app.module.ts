a528134f8c7d430f81a727c41e5209f532751daa
David Guevara <dasanguepa@gmail.com>
2026-09-22 16:15:53 -0500

    feat: import ProjectsModule into appModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,11 @@ import { PrismaModule } from './prisma/prisma.module';
 import { MaterialsModule } from './materials/materials.module';
 import { DepartmentsModule } from './departments/departments.module';
 import { CitiesModule } from './cities/cities.module';
+import { ProjectsModule } from './projects/projects.module';
+
 
 @Module({
-  imports: [PrismaModule, MaterialsModule, DepartmentsModule, CitiesModule],
+  imports: [PrismaModule, MaterialsModule, DepartmentsModule, CitiesModule, ProjectsModule],
   controllers: [AppController],
   providers: [AppService],
 })
